403e67ab2f74faa3a27e8b8bbe2201d5618fee2a
BryllieBrinkley <jibryllbrink@yahoo.com>
2024-08-22 14:59:49 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jibryll Brinkley on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
